e6d091e51185b25c9730a75b158741ad6f45d7b3
Kyle Chapman <kyleichapman@gmail.com>
2023-05-12 09:46:34 +0200

    Use correct hook for updating media object

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { BasePlugin, BaseComponent } from 'vatom-spaces-plugins'
+
 /**
  * This is the main entry point for your plugin.
  *
  * All information regarding plugin development can be found at
- * https://developer.vatom.com/plugins/plugins/
+ * https://developer.vatom.com/spaces/plugins-in-spaces/guide-create-plugin
  *
  * @license MIT
  * @author Vatom Inc.
@@ -17,18 +19,20 @@ export default class MediaButtonPlugin extends BasePlugin {
     /** Called on load */
     onLoad() {
 
+        // Register button as an attachable component
         this.objects.registerComponent(MediaButton, {
             id: 'media-button',
             name: 'Media Button',
-            description: 'Converts this object into a media plugin',
+            description: 'Converts this object into a media player.',
             settings: obj => [
-                { id: 'info', type: 'label', value: 'Settings' },
-                { id: 'select-name', name: 'Select Via Name', type: 'checkbox', help: 'Allows for selection of media player via its name rather than ID.'},
-                this.getComponentField(obj, 'media-button', 'select-name') == true ? {id: 'media-player-name', name: 'Media Player Name', type: 'select-item', help: 'Name of the media player object. Leaving this blank will default button to nearest media player (within 20 metres).'} :
-                { id: 'media-player-id', name: 'Media Player ID', type: 'input', help: 'ID of the media player object.Leaving this blank will default button to nearest media player (within 20 metres).'},
-                { id: 'media-source-id', name: 'Media Source URL', type: 'input', help:'URL for the media source you wish to play with this button.'},
-                { id: 'who-can-click', name: 'Who Can Press?', type: 'select', default: 'Everyone', values: ['Everyone', 'Admin Only'], help: 'Type of user who is allowed to click on the media button. Default is Everyone.' },
-                { id: 'event-name', name: 'Event Name', type: 'text', help: 'If set, this button will record analytics events for the attached video using the given event name' },
+                { id: 'info', type: 'label', value: 'Allows this object to control another media player.' },
+                { id: 'select-name', name: 'Select Via Name', type: 'checkbox', help: 'When enabled, it allows for selection of the media player by its name rather than identifier.' },
+                this.getComponentField(obj, 'media-button', 'select-name') == true
+                    ? { id: 'media-player-name', name: 'Media Player Name', type: 'select-item', help: 'Name of the media player object. Leaving this blank will default button to nearest media player (within 20 metres).' }
+                    : { id: 'media-player-id', name: 'Media Player ID', type: 'input', help: 'Identifier of the media player object. Leaving this blank will default button to nearest media player (within 20 metres).' },
+                { id: 'media-source-id', name: 'Media Source URL', type: 'input', help: 'URL for the media source you wish to play with this button.' },
+                { id: 'who-can-click', name: 'Who Can Use', type: 'select', default: 'Everyone', values: ['Everyone', 'Admin Only'], help: 'Type of user who is allowed to click on the media button. Default is Everyone.' },
+                { id: 'event-name', name: 'Event Name', type: 'text', help: 'If set, this button will record analytics events for the attached video using the given event name.' },
             ]
         })
 
@@ -36,73 +40,86 @@ export default class MediaButtonPlugin extends BasePlugin {
 
 }
 
-class MediaButton extends BaseComponent{
-    
+/**
+ * Component that allows an object to be used as a media button.
+ */
+class MediaButton extends BaseComponent {
+
+    /** Called when the button is clicked */
     async onClick() {
 
         // Stop if set to admin only and user is not admin
-        let isAdmin = await this.plugin.user.isAdmin()
-        if(this.getField('who-can-click') == "Admin Only" && !isAdmin){
+        const isAdmin = await this.plugin.user.isAdmin()
+        if (this.getField('who-can-click') == "Admin Only" && !isAdmin) {
             return
         }
-        
+
         // Get media button settings
-        let usingName = this.getField('select-name')
-        let mediaPlayerID = usingName ?  this.getField('media-player-name') : this.getField('media-player-id')
-        let mediaSourceID = this.getField('media-source-id')
+        const usingName = this.getField('select-name') == true
+        const mediaSourceURL = this.getField('media-source-id')
+        let mediaPlayerID = usingName ? this.getField('media-player-name') : this.getField('media-player-id')
 
+        // Attempt to find nearest media player if no identifier given
+        if (!mediaPlayerID) {
+            try {
+                const button = await this.plugin.objects.get(this.objectID)
+                const nearbyObjects = await this.plugin.objects.fetchInRadius(button.x, button.y, 20)
+                let foundDistance = 0
+                let mediaPlayer = null
 
-        // If no player ID specified attempt to find nearest one
-        if(!mediaPlayerID){
-            let button = await this.plugin.objects.get(this.objectID)
-            let nearbyObjects = await this.plugin.objects.fetchInRadius(button.x, button.y, 20)
-            let foundDistance = 0
-            let mediaPlayer = null
-            for(let object of nearbyObjects){
-                let position = {x: object.x || 0, y: object.y || 0}
-                let distance = Math.sqrt((position.x - button.x) ** 2 + (position.y - button.y) ** 2)
-                let isMediaPlayer = false
-                if(object.components){
-                    for(let component of object.components){
-                        if(component.id == "media-playback:media-source"){
-                            isMediaPlayer = true
+                // Find nearest object
+                for (let object of nearbyObjects) {
+                    let position = { x: object.x || 0, y: object.y || 0 }
+                    let distance = Math.sqrt((position.x - button.x) ** 2 + (position.y - button.y) ** 2)
+                    let isMediaPlayer = false
+
+                    // Check if this object has a media source component
+                    if (object.components) {
+                        isMediaPlayer = object.components.findIndex(comp => comp.id == 'media-playback:media-source') >= 0
+
+                        if ((!mediaPlayer || foundDistance > distance) && isMediaPlayer) {
+                            mediaPlayer = object
+                            foundDistance = distance
                         }
                     }
-                    if((!mediaPlayer || foundDistance > distance) && isMediaPlayer) {
-                        mediaPlayer = object
-                        foundDistance = distance
-                    }
                 }
-               
+
+                // Found a media player nearby, so use it
+                if (mediaPlayer) {
+                    mediaPlayerID = mediaPlayer.id
+                }
+            } catch (err) {
+                console.error('[MediaButton] Failed to find nearest media player.', err)
+                this.plugin.menus.alert('Failed to find nearest media player.', 'Error', 'error')
+                return
             }
-
-            if(mediaPlayer) mediaPlayerID = mediaPlayer.id
         }
 
-        // If both settings are present
-        if(mediaSourceID && mediaPlayerID) {
-            await this.plugin.hooks.trigger('media.presenter.setObjectProperties', { 
-                objectID: mediaPlayerID,
-                changes: {
-    
-                    // Set media source
-                    ['component:media-playback:media-source:src']: mediaSourceID,
-                    ['component:media-playback:media-source:event-name']: this.getField('event-name') || '',
-    
-                    // Sync command: Play immediately from the beginning
-                    public: {
-                        media_source_sync_action: 'play',
-                        media_source_sync_time: Date.now(),
-                        media_source_sync_nonce: Date.now(),
-                        media_source_sync_seek: 0
-                    }
-    
-                }
-            })
-        }
-        else{
+        // Cannot continue with relevant information
+        if (!mediaSourceURL || !mediaPlayerID) {
             this.plugin.menus.alert("No media source URL or media player ID was found for this button")
+            return
         }
+
+        // Update object
+        await this.plugin.hooks.trigger('plugins.media-playback.properties.set', {
+            objectID: mediaPlayerID,
+            changes: {
+
+                // Set media source
+                ['component:media-playback:media-source:src']: mediaSourceURL,
+                ['component:media-playback:media-source:event-name']: this.getField('event-name') || '',
+
+                // Sync command: Play immediately from the beginning
+                public: {
+                    media_source_sync_action: 'play',
+                    media_source_sync_time: Date.now(),
+                    media_source_sync_nonce: Date.now(),
+                    media_source_sync_seek: 0
+                }
+
+            }
+        })
 
     }
 }
